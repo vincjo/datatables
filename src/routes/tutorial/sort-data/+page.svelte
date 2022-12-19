7ca940b6288f781807871b7568dd1f4bc2f19b7b
vincjo <v.joseph3@gmail.com>
2022-12-19 15:56:42 +0100

    update tutorial

--- src/routes/tutorial/sort-data/+page.svelte
+++ src/routes/tutorial/sort-data/+page.svelte
@@ -7,7 +7,7 @@
 
 <h2>Sort data</h2>
 
-<p>When clicking on the <code>th</code> element, we want the column to be ordered, one move up, the next down.</p>
+<p>When clicking on the <code>th</code> element, we want the column to be sorted, one move up, the next down.</p>
 <p>We create a dedicated component <code>Th.svelte</code> to keep the logic well separated.</p> 
 <p>We will need to specify which object key will be sorted by passing the <code>orderBy</code> property to the component.</p>
 <Tutorial components={data.components} dataset={data.dataset} scrollY={false}>
