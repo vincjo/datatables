c168917f7ff0de7666918b6d34c0b00c100b46ca
vincjo <v.joseph3@gmail.com>
2022-12-22 17:23:25 +0100

    build: change base url

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
   kit: {
 		adapter: adapter({ out: 'build' }),
 		paths: {
-			base: process.env.NODE_ENV === 'development' ? '' : '/datatables',
+			base: process.env.NODE_ENV === 'development' ? '' : '/ssd',
 		},
   }
 };
