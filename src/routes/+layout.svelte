62f2b3289d4dfa7fb1d7cdf6f4d06ac8954c947f
vincjo <v.joseph3@gmail.com>
2022-12-23 15:47:09 +0100

    fix: home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,4 +29,9 @@
     section{position:absolute;top:0;bottom:0;right:0;width:70%;display:flex;justify-content:flex-start;overflow-y:auto;}
     article{padding:32px;width:1080px;min-width:800px;min-height:100vh;background:#fff;}
 
+    @media (max-width: 800px) { 
+        section{position:unset;width:100%;padding:16px 24px;}
+        article{padding:0;min-width:200px;}
+        nav{display:none;}
+    }
 </style>
