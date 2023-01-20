bdafd2ab52c1717e84044378f9fd2204cbc7efea
vincjo <v.joseph3@gmail.com>
2023-01-20 19:03:36 +0100

    style: ThFilter

--- src/lib/ThFilter.svelte
+++ src/lib/ThFilter.svelte
@@ -20,17 +20,18 @@
 
 
 <style>
+    th {
+        border-bottom: 1px solid #e0e0e0;
+    }
     input {
-        margin: 0;
+        margin: -1px 0 0 0;
         padding: 0;
         width: 100%;
         height: 24px;
         border: none;
-        border-bottom: 1px solid #e0e0e0;
         text-align: center;
         background:inherit;
         outline: none;
-        border-radius: 0;
         font-size: 14px;
     }
     input::placeholder {
@@ -39,6 +40,6 @@
         font-size:13px;
     }
     input:focus{
-        outline: 1px solid #bdbdbd;
+        outline: none;
     }
 </style>
