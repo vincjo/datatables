28ab0a165373c6426158c25f26ae4ba6c7466088
vincjo <v.joseph3@gmail.com>
2023-03-21 19:49:53 +0100

    add row selection example

---
+++ src/routes/row-selection/Main.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+    import myData from '$data/data'
+    import { Checkbox } from 'gros/form'
+    import {
+        DataHandler,
+        Datatable,
+        Th, ThFilter
+    } from '$lib/core'
+
+    const handler = new DataHandler(myData, { rowsPerPage: 50 })
+    const rows = handler.getRows()
+
+    let selected = []
+
+    const setSelected = (id) => {
+        if (selected.includes(id)) {
+            selected = selected.filter(item => item !== id)
+            return
+        }
+        selected = [id, ...selected]
+    }
+
+    const selectAll = () => {
+        if (selected.length === $rows.length) {
+            selected = []
+            return
+        }
+        selected = $rows.map( row => { return row.id })
+    }
+</script>
+
+
+<Datatable {handler}>
+    <table>
+        <thead>
+            <tr>
+                <th class="selection">
+                    <Checkbox on:click={selectAll}  checked={selected.length === $rows.length}/>
+                </th>
+                <Th {handler} orderBy="id">id</Th>
+                <Th {handler} orderBy="first_name">first_name</Th>
+                <Th {handler} orderBy="last_name">last_name</Th>
+            </tr>
+            <tr>
+                <th class="selection"/>
+                <ThFilter {handler} filterBy="id"/>
+                <ThFilter {handler} filterBy="first_name"/>
+                <ThFilter {handler} filterBy="last_name"/>
+            </tr>
+        </thead>
+        <tbody>
+        {#each $rows as row}
+            <tr class:active={selected.includes(row.id)}>
+                <td class="selection">
+                    <Checkbox on:click={() => setSelected(row.id)} checked={selected.includes(row.id)}/>
+                </td>
+                <td>{@html row.id ?? `<b>null</b>`}</td>
+                <td>{@html row.first_name ?? `<b>null</b>`}</td>
+                <td>{@html row.last_name ?? `<b>null</b>`}</td>
+            </tr>
+        {/each}
+        </tbody>
+    </table>
+</Datatable>
+
+
+
+<style>
+    thead{
+        background:#fff;
+    }
+    thead th.selection {
+        width: 48px;
+        padding-left: 16px;
+        border-bottom: 1px solid #e0e0e0;
+    }
+    tbody td{
+        padding:4px;
+    }
+    tbody tr:nth-child(even){
+        background:#fafafa;
+    }
+    tbody tr{
+        transition:all, 0.2s;
+    }
+    tbody tr:hover{
+        background:#f5f5f5;
+    }
+    tbody tr.active {
+        background: var(--primary-lighten-1)
+    }
+    td :global(b) {
+        font-weight: normal;
+        color: #bdbdbd;
+        font-family: JetBrains;
+        font-size: 11px;
+    }
+    td.selection {
+        padding-left: 16px;
+    }
+</style>
