c5b0724931f30c318d096d9bc07b755514b0fa41
Dmitrii Lavrov <d.lavrov@proscom.ru>
2023-05-12 18:24:38 +0200

    fix indentation

--- src/lib/DataHandler.ts
+++ src/lib/DataHandler.ts
@@ -10,157 +10,157 @@ import type { Internationalization } from '$lib';
 export type Params = { rowsPerPage?: number; i18n?: Internationalization };
 
 export default class DataHandler<T extends { [key: string]: unknown } = any> {
-  private context: Context<T>;
-  private rows: Rows<T>;
-  private pages: Pages;
-  private globalSearch: GlobalSearch;
-  private filters: Filters;
-  public i18n: Internationalization;
+	private context: Context<T>;
+	private rows: Rows<T>;
+	private pages: Pages;
+	private globalSearch: GlobalSearch;
+	private filters: Filters;
+	public i18n: Internationalization;
 
-  constructor(data: T[] = [], params: Params = { rowsPerPage: null }) {
-    this.i18n = this.translate(params.i18n);
-    this.context = new Context(data, params);
-    this.rows = new Rows(this.context);
-    this.pages = new Pages(this.context);
-    this.globalSearch = new GlobalSearch(this.context);
-    this.filters = new Filters(this.context);
-  }
+	constructor(data: T[] = [], params: Params = { rowsPerPage: null }) {
+		this.i18n = this.translate(params.i18n);
+		this.context = new Context(data, params);
+		this.rows = new Rows(this.context);
+		this.pages = new Pages(this.context);
+		this.globalSearch = new GlobalSearch(this.context);
+		this.filters = new Filters(this.context);
+	}
 
-  public setRows(data: T[]): void {
-    this.context.rawRows.set(data);
-    this.applySorting();
-  }
+	public setRows(data: T[]): void {
+		this.context.rawRows.set(data);
+		this.applySorting();
+	}
 
-  public getRows(): Readable<T[]> {
-    return this.context.rows;
-  }
+	public getRows(): Readable<T[]> {
+		return this.context.rows;
+	}
 
-  public select(value: any): void {
-    this.rows.select(value);
-  }
+	public select(value: any): void {
+		this.rows.select(value);
+	}
 
-  public getSelected(): Writable<any[]> {
-    return this.context.selected;
-  }
+	public getSelected(): Writable<any[]> {
+		return this.context.selected;
+	}
 
-  public selectAll(params: { selectBy?: string; scope?: 'all' | 'currentPage' } = {}): void {
-    this.context.selectScope.set(params.scope ?? 'all');
-    this.rows.selectAll(params.selectBy ?? null);
-  }
+	public selectAll(params: { selectBy?: string; scope?: 'all' | 'currentPage' } = {}): void {
+		this.context.selectScope.set(params.scope ?? 'all');
+		this.rows.selectAll(params.selectBy ?? null);
+	}
 
-  public isAllSelected(): Readable<boolean> {
-    return this.context.isAllSelected;
-  }
+	public isAllSelected(): Readable<boolean> {
+		return this.context.isAllSelected;
+	}
 
-  public getRowCount(): Readable<{ total: number; start: number; end: number }> {
-    return this.context.rowCount;
-  }
+	public getRowCount(): Readable<{ total: number; start: number; end: number }> {
+		return this.context.rowCount;
+	}
 
-  public getRowsPerPage(): Writable<number | null> {
-    return this.context.rowsPerPage;
-  }
+	public getRowsPerPage(): Writable<number | null> {
+		return this.context.rowsPerPage;
+	}
 
-  public sort(orderBy: Parameters<Rows<T>['sort']>[0]): void {
-    this.setPage(1);
-    this.rows.sort(orderBy);
-  }
+	public sort(orderBy: Parameters<Rows<T>['sort']>[0]): void {
+		this.setPage(1);
+		this.rows.sort(orderBy);
+	}
 
-  public applySorting(
-    params: { orderBy: Function | string; direction?: 'asc' | 'desc' | null } | null = null
-  ): void {
-    this.rows.applySorting(params);
-  }
+	public applySorting(
+		params: { orderBy: Function | string; direction?: 'asc' | 'desc' | null } | null = null
+	): void {
+		this.rows.applySorting(params);
+	}
 
-  public sortAsc(orderBy: Function | string): void {
-    this.setPage(1);
-    this.rows.sortAsc(orderBy);
-  }
+	public sortAsc(orderBy: Function | string): void {
+		this.setPage(1);
+		this.rows.sortAsc(orderBy);
+	}
 
-  public sortDesc(orderBy: ((row: T) => T[keyof T]) | keyof T): void {
-    this.setPage(1);
-    this.rows.sortDesc(orderBy);
-  }
+	public sortDesc(orderBy: ((row: T) => T[keyof T]) | keyof T): void {
+		this.setPage(1);
+		this.rows.sortDesc(orderBy);
+	}
 
-  public getSorted(): Writable<{ identifier: string | null; direction: 'asc' | 'desc' | null }> {
-    return this.context.sorted;
-  }
+	public getSorted(): Writable<{ identifier: string | null; direction: 'asc' | 'desc' | null }> {
+		return this.context.sorted;
+	}
 
-  public search(value: string, scope: string[] = null): void {
-    this.globalSearch.set(value, scope);
-  }
+	public search(value: string, scope: string[] = null): void {
+		this.globalSearch.set(value, scope);
+	}
 
-  public clearSearch(): void {
-    this.globalSearch.remove();
-  }
+	public clearSearch(): void {
+		this.globalSearch.remove();
+	}
 
-  public filter(
-    value: string,
-    filterBy: ((row: any) => string | number | boolean) | string,
-    comparator: Function = null
-  ): void {
-    return this.filters.set(value, filterBy, comparator);
-  }
+	public filter(
+		value: string,
+		filterBy: ((row: any) => string | number | boolean) | string,
+		comparator: Function = null
+	): void {
+		return this.filters.set(value, filterBy, comparator);
+	}
 
-  public clearFilters(): void {
-    this.filters.remove();
-  }
+	public clearFilters(): void {
+		this.filters.remove();
+	}
 
-  public getPages(params = { ellipsis: false }): Readable<number[]> {
-    if (params.ellipsis) {
-      return this.context.pagesWithEllipsis;
-    }
-    return this.context.pages;
-  }
+	public getPages(params = { ellipsis: false }): Readable<number[]> {
+		if (params.ellipsis) {
+			return this.context.pagesWithEllipsis;
+		}
+		return this.context.pages;
+	}
 
-  public getPageCount(): Readable<number> {
-    return this.context.pageCount;
-  }
+	public getPageCount(): Readable<number> {
+		return this.context.pageCount;
+	}
 
-  public getPageNumber(): Readable<number> {
-    return this.context.pageNumber;
-  }
+	public getPageNumber(): Readable<number> {
+		return this.context.pageNumber;
+	}
 
-  public setPage(value: number | 'previous' | 'next'): void {
-    switch (value) {
-      case 'previous':
-        return this.pages.previous();
-      case 'next':
-        return this.pages.next();
-      default:
-        return this.pages.goTo(value as number);
-    }
-  }
+	public setPage(value: number | 'previous' | 'next'): void {
+		switch (value) {
+			case 'previous':
+				return this.pages.previous();
+			case 'next':
+				return this.pages.next();
+			default:
+				return this.pages.goTo(value as number);
+		}
+	}
 
-  public getTriggerChange(): Writable<number> {
-    return this.context.triggerChange;
-  }
+	public getTriggerChange(): Writable<number> {
+		return this.context.triggerChange;
+	}
 
-  public translate(i18n: Internationalization): Internationalization {
-    return {
-      ...{
-        search: 'Search...',
-        show: 'Show',
-        entries: 'entries',
-        filter: 'Filter',
-        rowCount: 'Showing {start} to {end} of {total} entries',
-        noRows: 'No entries found',
-        previous: 'Previous',
-        next: 'Next'
-      },
-      ...i18n
-    };
-  }
+	public translate(i18n: Internationalization): Internationalization {
+		return {
+			...{
+				search: 'Search...',
+				show: 'Show',
+				entries: 'entries',
+				filter: 'Filter',
+				rowCount: 'Showing {start} to {end} of {total} entries',
+				noRows: 'No entries found',
+				previous: 'Previous',
+				next: 'Next'
+			},
+			...i18n
+		};
+	}
 
-  /**
-   * Deprecated
-   * use setRows() instead
-   */
-  public update(data: any[]): void {
-    console.log(
-      '%c%s',
-      'color:#e65100;background:#fff3e0;font-size:12px;border-radius:4px;padding:4px;text-align:center;',
-      `DataHandler.update(data) method is deprecated. Please use DataHandler.setRows(data) instead`
-    );
-    this.context.rawRows.set(data);
-  }
+	/**
+	 * Deprecated
+	 * use setRows() instead
+	 */
+	public update(data: any[]): void {
+		console.log(
+			'%c%s',
+			'color:#e65100;background:#fff3e0;font-size:12px;border-radius:4px;padding:4px;text-align:center;',
+			`DataHandler.update(data) method is deprecated. Please use DataHandler.setRows(data) instead`
+		);
+		this.context.rawRows.set(data);
+	}
 }
--- src/lib/core/Handlers/GlobalSearch.ts
+++ src/lib/core/Handlers/GlobalSearch.ts
@@ -2,23 +2,23 @@ import type Context from '../Context';
 import type { Writable } from 'svelte/store';
 
 export default class Search {
-  private globalSearch: Writable<{ value: string | null; scope: string[] | null }>;
+	private globalSearch: Writable<{ value: string | null; scope: string[] | null }>;
 
-  constructor(context: Context) {
-    this.globalSearch = context.globalSearch;
-  }
+	constructor(context: Context) {
+		this.globalSearch = context.globalSearch;
+	}
 
-  public set(value: string, scope: string[] = null): void {
-    this.globalSearch.update((store) => {
-      store = {
-        value: value ?? '',
-        scope: scope ?? null
-      };
-      return store;
-    });
-  }
+	public set(value: string, scope: string[] = null): void {
+		this.globalSearch.update((store) => {
+			store = {
+				value: value ?? '',
+				scope: scope ?? null
+			};
+			return store;
+		});
+	}
 
-  public remove(): void {
-    this.globalSearch.set({ value: null, scope: null });
-  }
+	public remove(): void {
+		this.globalSearch.set({ value: null, scope: null });
+	}
 }
