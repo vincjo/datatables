b91885ab076a044e6383ffda8cb37fdd1987494d
vincjo <v.joseph3@gmail.com>
2024-01-19 17:31:50 +0100

    feat: add example

--- src/routes/(local)/test/filter2/+page.server.ts
+++ src/routes/(local)/test/filter2/+page.server.ts
@@ -3,12 +3,18 @@ import * as fs from 'fs'
 export const load = async () => {
     const path = './src/routes/(local)/test'
     const lib = './src/lib/local'
-    const page = 'filter'
+    const page = 'filter2'
     return {
         components: [
             {
                 name: 'Main.svelte',
                 code: fs.readFileSync(`${path}/${page}/Main.svelte`).toString('utf-8'),
+                components: [
+                    {
+                        name: 'ThFilter.svelte',
+                        code: fs.readFileSync(`${path}/${page}/ThFilter.svelte`).toString('utf-8')
+                    },
+                ]
             }
         ]
     }
