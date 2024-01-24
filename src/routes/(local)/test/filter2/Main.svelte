be9bcedb31e3399e4fb27987adfbf1f9134366e5
vincjo <v.joseph3@gmail.com>
2024-01-24 08:45:35 +0100

    feat: add link to skeleton components for ssd

--- src/routes/(local)/test/filter2/Main.svelte
+++ src/routes/(local)/test/filter2/Main.svelte
@@ -1,53 +1,53 @@
 <script>
-	import { DataHandler, Th } from '$lib';
-	import ThFilter from './ThFilter.svelte';
+    import { DataHandler, Th } from '$lib';
+    import ThFilter from './ThFilter.svelte';
 
 
-	const data = [
-		{
-			name: 'Mario',
-			status: 'DRAFT',
-			ammount: 41
-		},
-		{
-			name: 'George',
-			status: 'OPEN',
-			ammount: 54
-		},
-		{
-			name: 'Mario',
-			status: 'OPEN',
-			ammount: 31
-		}
-	];
+    const data = [
+        {
+            name: 'Mario',
+            status: 'DRAFT',
+            ammount: 41
+        },
+        {
+            name: 'George',
+            status: 'OPEN',
+            ammount: 54
+        },
+        {
+            name: 'Mario',
+            status: 'OPEN',
+            ammount: 31
+        }
+    ];
 
-	const handler = new DataHandler(data, { rowsPerPage: 3 });
-	const rows = handler.getRows();
+    const handler = new DataHandler(data, { rowsPerPage: 3 });
+    const rows = handler.getRows();
 
-	const keys = Object.keys(data[0]); // ['name', 'status', 'ammount']
+    const keys = Object.keys(data[0]); // ['name', 'status', 'ammount']
 
 </script>
 
 <table>
-	<thead>
-		<tr>
-			{#each keys as key, i}
-				<Th {handler} orderBy={key} identifier={'th' + i}>{key}</Th>
-			{/each}
-		</tr>
-		<tr>
-			{#each keys as key}
-				<ThFilter {handler} {key} />
-			{/each}
-		</tr>
-	</thead>
-	<tbody>
-		{#each $rows as row}
-			<tr>
-				{#each keys as key}
-					<td>{row[key]}</td>
-				{/each}
-			</tr>
-		{/each}
-	</tbody>
+    <thead>
+        <tr>
+            {#each keys as key, i}
+                <Th {handler} orderBy={key} identifier={'th' + i}>{key}</Th>
+            {/each}
+        </tr>
+        <tr>
+            {#each keys as key}
+                <ThFilter {handler} {key} />
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        {#each $rows as row}
+            <tr>
+                {#each keys as key}
+                    <td>{row[key]}</td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
 </table>
