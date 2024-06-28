2456d72dff0d651e4b071c0cfef83242ea198572
vincjo <v.joseph3@gmail.com>
2024-06-28 13:45:56 +0200

    track svelte language tool error

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Component from './Component.svelte'
+    const content = 'test'
+</script>
+
+<Component content={content}>
+    {#snippet header()}
+        <p>header</p>
+    {/snippet}
+
+    <p>article</p>
+
+    {#snippet footer()}
+        <p>footer</p>
+    {/snippet}
+</Component>
---
+++ src/routes/test/Component.svelte
@@ -0,0 +1,36 @@
+<script>
+    let { footer, header, children, content } = $props()
+</script>
+
+<section>
+    <header>
+        {@render header()}
+    </header>
+
+    <article>
+        {@render children()} {content}
+    </article>
+
+    <footer>
+        {@render footer()}
+    </footer>
+
+</section>
+
+<style>
+    section {
+        border-radius: 8px;
+    }
+    header, article, footer {
+        height: 56px;
+    }
+    header {
+        background: blueviolet;
+    }
+    footer {
+        background: blueviolet
+    }
+    article {
+        background: burlywood;
+    }
+</style>
