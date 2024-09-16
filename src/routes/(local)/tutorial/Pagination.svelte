f1530d5b75112ff1dace12d9f879e8087429be96
vincjo <v.joseph3@gmail.com>
2024-09-16 15:40:47 +0200

    fix: pagination tutorial

--- src/routes/(local)/tutorial/Pagination.svelte
+++ src/routes/(local)/tutorial/Pagination.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import type { DataHandler } from '$lib/local'
     export let handler: DataHandler
-    const currentPage = handler.getCurrentPage()
+    const pageNumber = handler.getPageNumber()
     const pageCount = handler.getPageCount()
     const pages = handler.getPages({ ellipsis: true })
 </script>
@@ -9,7 +9,7 @@
 <section>
     <button
         type="button"
-        class:disabled={$currentPage === 1}
+        class:disabled={$pageNumber === 1}
         on:click={() => handler.setPage('previous')}
     >
         Previous
@@ -17,7 +17,7 @@
     {#each $pages as page}
         <button
             type="button"
-            class:active={$currentPage === page}
+            class:active={$pageNumber === page}
             class:ellipse={page === null}
             on:click={() => handler.setPage(page)}
         >
@@ -26,7 +26,7 @@
     {/each}
     <button
         type="button"
-        class:disabled={$currentPage === $pageCount}
+        class:disabled={$pageNumber === $pageCount}
         on:click={() => handler.setPage('next')}
     >
         Next
