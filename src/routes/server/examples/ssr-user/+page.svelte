9b4ed9a0ed63b86f4f1fcb3e051515b8a73ef2d1
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2024-10-19 15:30:30 -0300

    docs: ssr exemple

---
+++ src/routes/server/examples/ssr-user/+page.svelte
@@ -0,0 +1,87 @@
+
+<script lang="ts">
+    import { navigating } from '$app/stores';
+    import type { PageData } from './$types'
+  
+    let { data }: { data: PageData } = $props()
+    import {
+      TableHandler,
+      Datatable,
+      ThSort,
+      ThFilter,
+      Pagination,
+      RowsPerPage,
+      Search,
+      Th,
+      type State,
+    } from '$lib/src/server'
+    import { SSRFilters } from './params.svelte';
+  
+    const filters = new SSRFilters()
+  
+    const table = new TableHandler(data.rows, {
+      rowsPerPage: 10,
+      totalRows: data.count,
+    })
+  
+    table.load(async s => {
+      console.log(s)
+      filters.fromState(s)
+      await $navigating?.complete
+      return data.rows
+    })
+  </script>
+  
+  <Datatable {table}>
+    {#snippet header()}
+      <Search {table} />
+      <RowsPerPage {table} />
+    {/snippet}
+    <table>
+        <thead>
+            <tr>
+                <Th>ID</Th>
+                <Th>Avatar</Th>
+                <Th>Fristname</Th>
+                <Th>Lastname</Th>
+                <Th>Age</Th>
+                <Th>Gender</Th>
+                <Th>Height / Weight</Th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each table.rows as row}
+                <tr>
+                    <td>{row.id}</td>
+                    <td>
+                        <img src="{row.image}" alt="avatar" />
+                    </td>
+                    <td>{row.firstName}</td>
+                    <td>{row.lastName}</td>
+                    <td>{row.age}</td>
+                    <td>{row.gender}</td>
+                    <td>
+                        {String(row.height / 100).substring(0, 4)}m / {String(row.weight).substring(0, 4)}kg
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+    {#snippet footer()}
+      <div></div>
+      <Pagination {table} />
+    {/snippet}
+  </Datatable>
+  
+  <style>
+    b {
+      color: var(--r-primary);
+      font-weight: normal;
+      line-height: 16px;
+      white-space: break-spaces;
+    }
+    span {
+      padding-left: 8px;
+    }
+  </style>
+  
---
+++ src/routes/server/examples/ssr-user/params.svelte.ts
@@ -0,0 +1,75 @@
+import { page, navigating } from "$app/stores";
+import { get } from "svelte/store";
+import { goto } from "$app/navigation";
+import type { State } from "$lib/server";
+
+export class SSRFilters {
+  Filters = $derived.by(() => get(page).url);
+
+  constructor() {
+    console.log("Filters", this.Filters);
+  }
+
+  get(name: string) {
+    return this.Filters.searchParams.get(name);
+  }
+
+  update(filters: Record<string, string>) {
+    const url = new URL(this.Filters);
+    Object.entries(filters).forEach(([name, value]) => {
+      if (!value) {
+        url.searchParams.delete(name);
+      }
+      if (value !== "") url.searchParams.set(name, value);
+      else url.searchParams.delete(name);
+    });
+
+    if (typeof window !== "undefined") {
+      goto(url, { keepFocus: true });
+    }
+  }
+
+  fromState(s: State) {
+    const filters = s.filters ?? [];
+    const sort = s.sort;
+    const page = s.currentPage;
+    const rowsPerPage = s.rowsPerPage;
+
+
+    const url = new URL(this.Filters);
+    for (const { field, value } of filters) {
+      console.log("field", field, value);
+      url.searchParams.set(field.toString(), value.toString());
+    }
+    if (sort?.field && sort?.direction) {
+      console.log("sort", sort);
+      url.searchParams.set("sort_id", sort.field?.toString());
+      url.searchParams.set("sort_direction", sort.direction);
+    }
+    if (page) {
+      url.searchParams.set("page", page.toString());
+    }
+    if (rowsPerPage) {
+      url.searchParams.set("pageSize", rowsPerPage.toString());
+    }
+
+    if (typeof window !== "undefined") {
+      goto(url, { keepFocus: true });
+    }
+  }
+
+  clear(...params: string[]) {
+    const url = new URL(this.Filters);
+    params.forEach((p) => url.searchParams.delete(p));
+
+    if (typeof window !== "undefined") {
+      goto(url, { keepFocus: true });
+    }
+  }
+
+  isFiltered(...params: string[]) {
+    return (
+      params.length > 0 && params.some((p) => this.Filters.searchParams.has(p))
+    );
+  }
+}
